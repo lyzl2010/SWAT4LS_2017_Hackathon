8fa79e0a0b3e670132e41897a5b93dc9a642ab3f
Md. Rezaul Karim <reza_cse_du@yahoo.com>
2017-11-29 17:30:42 +0100

    Update SubTypePrediction_DNAMet_ER_with_MLP.scala

--- CancerPredictionApacheSpark/src/main/scala/org/fit/BreastCancer/SubTypePrediction/SubTypePrediction_DNAMet_ER_with_MLP.scala
+++ CancerPredictionApacheSpark/src/main/scala/org/fit/BreastCancer/SubTypePrediction/SubTypePrediction_DNAMet_ER_with_MLP.scala
@@ -31,7 +31,7 @@ object SubTypePrediction_DNAMet_ER_with_MLP {
       .option("delimiter", ",")
       .format("com.databricks.spark.csv")
       .option("header", "false")
-      .load("data/input_csv/type/DNA_Methylation/input_csv_met_type_er.csv")
+      .load("data/input_csv/type/DNA_Methylation/input_csv_met_type_er.csv") // don't forget to provide the correct path
 
     inputDataDF.printSchema()
 
@@ -40,7 +40,7 @@ object SubTypePrediction_DNAMet_ER_with_MLP {
       .option("delimiter", "\n")
       .format("com.databricks.spark.csv")
       .option("header", "false")
-      .load("data/labels_csv/type/label_type_er.csv")
+      .load("data/labels_csv/type/label_type_er.csv") // don't forget to provide the correct path
 
     val LabelDF = inputLabelDF.withColumnRenamed("_c0", "label")
     inputLabelDF.show()
